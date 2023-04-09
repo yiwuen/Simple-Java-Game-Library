d244c1e78966d45e2f742897f50c2326ea9c2e0e
joz <74896557+yiwuen@users.noreply.github.com>
2023-04-08 21:09:24 -0700

    Update MySJGLDemo.java

--- examples/MySJGLDemo.java
+++ examples/MySJGLDemo.java
@@ -5,15 +5,18 @@ import com.sjgl.utils.*;
 
 public class MySJGLDemo extends Application {
 
+	private static final long serialVersionUID = -1L;
+
 	public MySJGLDemo() {
 		SJGL_CreateWindow(850, 580, "My SJGL Window", this).display(WindowUtils.TERMINATE_WINDOW, true, true);
 	}
 
+	@Override
 	public void render() {
 		SJGL_Color3f(0.0f, 0.0f, 0.0f);
-		SJGL_Rect(0, 0, getWidth(), getHeight());
+		SJGL_Background();
 	}
-
+	
 	public static void main(String[] args) {
 		new MySJGLDemo().launch(args);
 	}
